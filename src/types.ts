52cb52618e70a1d68f48fe1586a00b30b1c4ceec
Sean Gransee <sean.gransee@gmail.com>
2018-11-14 20:47:21 -0600

    start adding examples to types

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,24 @@
 export type Scalar = string | boolean | number | null
 
+// { name: () => "Michael Scott" }
 export interface ObjectType {
   [field: string]: Resolver
 }
+
+// () => "Michael Scott"
 export type Resolver = () => ResolverValue
+
+// { name: () => "Michael Scott" }
+// [{ name: () => "Michael Scott" }, { name: () => "Dwight Schrute" }]
+// 51
+// [51, 52]
 export type ResolverValue = ObjectType | ObjectType[] | Scalar | Scalar[]
 
+// { name: "Michael Scott" }
 export interface ObjectTypeMock {
   [field: string]: MockValue
 }
+
 export interface DeclaredFunction {
   "function()": string
   args: Arguments
